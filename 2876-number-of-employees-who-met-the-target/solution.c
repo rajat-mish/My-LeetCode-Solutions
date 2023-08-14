1c14d976f2f7dd41d22bf509456e59cae85dd9c9
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2023-08-14 20:59:38 +0000

    Sync LeetCode submission Runtime - 6 ms (0.00%), Memory - 5.7 MB (100.00%)

---
+++ 2876-number-of-employees-who-met-the-target/solution.c
@@ -0,0 +1,8 @@
+int numberOfEmployeesWhoMetTarget(int* hours, int hoursSize, int target){
+int count=0;
+for(int i=0;i<hoursSize;i++){
+    if(hours[i]>=target){
+        count++;
+    }
+}return count;
+}
