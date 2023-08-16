602967455b3891808713dcd551591d4b5d8f7edb
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2023-08-16 15:54:31 +0000

    Sync LeetCode submission Runtime - 594 ms (14.49%), Memory - 7 MB (100.00%)

---
+++ 2042-maximum-product-difference-between-two-pairs/solution.c
@@ -0,0 +1,10 @@
+int maxProductDifference(int* nums, int numsSize){
+for(int i=1;i<numsSize;i++){
+    int j=i-1;
+    int x=nums[i];
+    while(j>=0 && nums[j]>x){
+        nums[j+1]=nums[j];
+        j--;
+    }nums[j+1]=x;
+}return((nums[numsSize-1]*nums[numsSize-2])-(nums[0]*nums[1]));
+}
