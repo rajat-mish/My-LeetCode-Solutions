aeca4e871fe58d40726571c7b534fa2bc097b228
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2023-08-19 20:31:55 +0000

    Sync LeetCode submission Runtime - 7 ms (0.00%), Memory - 6.4 MB (100.00%)

---
+++ 1950-sign-of-the-product-of-an-array/solution.c
@@ -0,0 +1,17 @@
+int arraySign(int* nums, int numsSize){
+int count=0;
+for(int i=0;i<numsSize;i++){
+    if(nums[i]==0){
+        return 0;
+    }
+   else if(nums[i]<0){
+        count++;
+    }
+}
+if(count%2==0){
+    return 1;
+}
+else{
+    return -1;
+}
+}
