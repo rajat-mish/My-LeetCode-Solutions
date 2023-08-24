c61fea22288a9ced4f35867f447001b94aeaf98e
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2023-08-24 11:24:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.5 MB (99.99%)

---
+++ 0033-search-in-rotated-sorted-array/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int t=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                t=i;
+            }
+          
+        }
+         return t; 
+    }
+}
