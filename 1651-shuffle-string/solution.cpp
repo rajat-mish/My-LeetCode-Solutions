82ca93454fcd7922fb78695b2c4ad1c7281d1c2f
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2023-10-06 08:35:06 +0000

    Sync LeetCode submission Runtime - 12 ms (4.65%), Memory - 15.5 MB (100.00%)

---
+++ 1651-shuffle-string/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) { int n= s.size();
+        string ans=s;
+        for(int i=0;i<s.size();i++){
+           //ans.push_back(s[indices[i]]);
+           ans[indices[i]]=s[i];
+        }
+        return ans;
+    }
+};
