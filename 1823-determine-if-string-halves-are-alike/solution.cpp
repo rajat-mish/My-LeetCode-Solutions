ff437050a85140f357a51f9d971d6cc01d70e184
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2023-10-21 08:01:19 +0000

    Sync LeetCode submission Runtime - 4 ms (4.75%), Memory - 7.3 MB (100.00%)

---
+++ 1823-determine-if-string-halves-are-alike/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool halvesAreAlike(string s) { int count1=0;
+    int count2=0;
+        string a=s.substr(0,s.size()/2);
+        string b=s.substr((s.size()/2),s.size());
+      for(int i=0;i<a.size();i++){
+          if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U'){
+              count1++;
+          }
+      }
+
+
+      for(int i=0;i<b.size();i++){
+          if(b[i]=='a'||b[i]=='e'||b[i]=='i'||b[i]=='o'||b[i]=='u'||b[i]=='A'||b[i]=='E'||b[i]=='I'||b[i]=='O'||b[i]=='U'){
+              count2++;
+          }
+      }
+      if(count1==count2){
+          return true;
+      }
+      else{
+          return false;
+      }
+
+    }
+};
