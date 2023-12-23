7a17d8f8403cc74eff5243fae255a8dfc479b2e7
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2023-12-23 09:20:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.2 MB (100.00%)

---
+++ 1580-shuffle-the-array/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) { vector<int>v;
+     int i=0;
+     int j=n;
+     while(i<n && j<nums.size()){
+         v.push_back(nums[i]);
+         v.push_back(nums[j]);
+         i++;j++;
+      
+     }
+     return v;
+    }
+};
