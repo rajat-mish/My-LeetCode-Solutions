4e3ec8317edefbfa900290586dadf1148a31d85d
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2023-12-28 08:14:17 +0000

    Sync LeetCode submission Runtime - 65 ms (5.38%), Memory - 71 MB (100.00%)

---
+++ 2767-maximum-sum-with-exactly-k-elements-/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximizeSum(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int sum=0;
+        for(int i=0;i<k;i++){
+          sum+=nums[nums.size()-1];
+          nums[nums.size()-1]+=1;
+        }
+        return sum;
+    }
+};
