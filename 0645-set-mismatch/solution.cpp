ec44909f5870a3b421bd80257a53c880bccbc506
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2024-01-09 07:08:06 +0000

    Sync LeetCode submission Runtime - 20 ms (29.86%), Memory - 21.7 MB (100.00%)

---
+++ 0645-set-mismatch/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& arr) {
+        vector<int>v;
+        int n=arr.size();
+        int i=0;
+    while(i<n){
+        int ci=arr[i]-1; //correct index of any element
+        if(i==ci || arr[ci]==arr[i]){
+            i++;
+        }
+        else{
+            swap(arr[i],arr[ci]);
+        }
+
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]!=i+1){
+            v.push_back(arr[i]);
+            v.push_back(i+1);
+        }
+    }
+    return v;
+    }
+};
