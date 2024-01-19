9de8487828efb4df1100f74607316b396cb10af0
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2024-01-19 18:03:52 +0000

    Sync LeetCode submission Runtime - 15 ms (12.39%), Memory - 6.3 MB (100.00%)

---
+++ 0724-find-pivot-index/solution.c
@@ -0,0 +1,21 @@
+int pivotIndex(int* nums, int numsSize) {
+    int left[numsSize];
+    int right[numsSize];
+left[0]=nums[0];
+right[numsSize-1]=nums[numsSize-1];
+
+    for(int i=1;i<numsSize;i++){
+        left[i]=nums[i]+left[i-1];
+    }
+    for(int i=numsSize-2;i>=0;i--){
+        right[i]=right[i+1]+nums[i];
+    }
+
+    for(int i=0;i<numsSize;i++){
+        if(right[i]==left[i]){
+            return i;
+        }
+    }
+    return -1;
+
+}
