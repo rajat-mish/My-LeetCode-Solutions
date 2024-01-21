a508819ac84b4a7b9cfb2ee9148d5ea8ae8ed1ea
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2024-01-21 11:38:32 +0000

    Sync LeetCode submission Runtime - 4 ms (1.48%), Memory - 13.4 MB (100.00%)

---
+++ 0086-partition-list/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+          ListNode*lo=new   ListNode(23);
+            ListNode*hi=new   ListNode(9);
+              ListNode*temp=head;
+                ListNode*tl=lo;
+                  ListNode*th=hi;
+                  while(temp!=NULL){
+                      if(temp->val<x){
+                          tl->next=temp;
+                          temp=temp->next;
+                          tl=tl->next;
+                      }
+                      else{
+                           th->next=temp;
+                          temp=temp->next;
+                          th=th->next;
+                      }
+                  }
+                tl->next=hi->next;
+                  th->next=NULL;
+
+                  return lo->next;
+
+    }
+};
