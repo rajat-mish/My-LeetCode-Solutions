92d0b70db677ddafe572ed257b80c981160575ec
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2024-02-13 15:41:08 +0000

    Sync LeetCode submission Runtime - 58 ms (6.01%), Memory - 26.7 MB (33.77%)

--- 2231-find-first-palindromic-string-in-the-array/solution.cpp
+++ 2231-find-first-palindromic-string-in-the-array/solution.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     string firstPalindrome(vector<string>& words) {
-        for(int i = 0; i < words.size(); i++) {
-            string reversed = words[i];
-            reverse(reversed.begin(), reversed.end());
-            if(words[i] == reversed) {
+        for(int i=0;i<words.size();i++){
+           
+            string s=words[i];
+            reverse(words[i].begin(),words[i].end());
+            if(words[i]==s){
                 return words[i];
             }
         }
         return "";
     }
 };
-
