463537b063b28b0f18e694f1a14836de2912c82e
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2024-03-26 09:13:43 +0000

    Sync LeetCode submission Runtime - 56 ms (5.27%), Memory - 37.6 MB (39.53%)

--- 0658-find-k-closest-elements/solution.cpp
+++ 0658-find-k-closest-elements/solution.cpp
@@ -1,78 +1,16 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<int>v(k); //store the answer
-
-        if(x<arr[0]){
-            for(int i=0;i<k;i++){
-                v[i]=arr[i];
-            }
-            return v;
+        pair<int,int>p;
+        vector<int>v;
+        priority_queue<pair<int,int>>pq;
+        for(int i=0;i<arr.size();i++){
+           pq.push({abs(arr[i]-x),arr[i]});
+           if(pq.size()>k)pq.pop();
         }
-
-        if(x>arr[arr.size()-1]){
-            int i=arr.size()-1;
-        int j=k-1;
-        while(j>=0){
-            v[j]=arr[i];
-            i--;
-            j--;
-        }
-        //sort(v.begin(),v.end());
-        return v;
-        }
-        int l=0;
-        int h=arr.size()-1;
-        int m=-1;
-        int t=0; //representing current index of answer
-        bool flag=false;  //element is present or not
-        while(l<=h){
-             m=l+(h-l)/2;
-
-            if(arr[m]==x){ flag=true;
-            v[t]=arr[m];
-            t++;
-               break;
-
-                
-            }
-            else if(arr[m]>x){
-                h=m-1;
-            }
-            else{
-                l=m+1;
-            }
-        }
-        int lb=h;
-        int ub=l;
-
-        if(flag==true){
-            lb=m-1;
-            ub=m+1;
-        }
-
-        while(t<k && lb>=0 && ub<arr.size()){
-           int d1=abs(x-arr[lb]);
-           int d2=abs(x-arr[ub]);
-           if(d1<=d2){
-               v[t]=arr[lb--];
-
-           }
-           else{
-               v[t]=arr[ub++];
-           }
-           t++;
-        }
-        if(lb<0){
-            while(t<k){
-                 v[t]=arr[ub++];
-                 t++;
-            }
-        }
-        if(ub>=arr.size()){
-            while(t<k){
-             v[t]=arr[lb--];
-             t++;}
+        while(pq.size()>0){
+          v.push_back(pq.top().second);
+          pq.pop();
         }
         sort(v.begin(),v.end());
         return v;
