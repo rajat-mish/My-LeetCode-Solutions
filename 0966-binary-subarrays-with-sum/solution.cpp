e15f80b46447f7429d75a658b99439e76fbbeb45
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2024-03-29 07:21:03 +0000

    Sync LeetCode submission Runtime - 27 ms (11.12%), Memory - 39.4 MB (10.91%)

---
+++ 0966-binary-subarrays-with-sum/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int k) {
+       vector<int>pre(nums.size(),0);
+       pre[0]=nums[0];
+       for(int i=1;i<nums.size();i++){
+        pre[i]=pre[i-1]+nums[i];
+       }
+       unordered_map<int,int>m;
+       int count=0;
+       for(int i=0;i<pre.size();i++){
+        if(pre[i]==k)count++;
+        int rem=pre[i]-k;
+        if(m.find(rem)!=m.end())count+=m[rem];
+        m[pre[i]]++;
+       }
+       return count;
+    }
+};
