3a32df7a504af0830c9dc4b152a0b27402bb941a
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2024-07-14 11:15:09 +0000

    Sync LeetCode submission Runtime - 4 ms (0.59%), Memory - 9.4 MB (100.00%)

--- 0198-house-robber/solution.cpp
+++ 0198-house-robber/solution.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
+// bottom up approach
 vector<int>dp;
-int fun(vector<int>v,int i){
-    if(i==v.size()-1)return v[i];
-    if(i==v.size()-2)return max(v[i],v[i+1]);
-    if(dp[i]!=-1)return dp[i];
-    return dp[i]=max((v[i]+fun(v,i+2)),0+fun(v,i+1));
+int fun(vector<int>&nums,int i){
+    
+    int n=nums.size();
+    if(n==1)return nums[0];
+    dp.clear();
+    dp.resize(105);
+    dp[n-1]=nums[n-1];
+    dp[n-2]=max(nums[n-1],nums[n-2]);
+    for(int i=n-3;i>=0;i--){
+        dp[i]=max(nums[i]+dp[i+2],dp[i+1]);
+    }
+    return dp[0];
 }
     int rob(vector<int>& nums) {
-        dp.clear();
-        dp.resize(105,-1);
-        return fun(nums,0);
+       return  fun(nums,0);
     }
 };
