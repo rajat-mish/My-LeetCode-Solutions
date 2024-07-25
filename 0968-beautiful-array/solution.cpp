b5d833739d26e15a0a9fcdbb8c615603ceb1e52d
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2024-07-25 10:16:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (100.00%)

---
+++ 0968-beautiful-array/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> beautifulArray(int n) {
+        vector<int> result = {1}; 
+        
+        while (result.size() < n) {
+            vector<int> temp;
+            // odd part
+            for (int num : result) {
+                if (num * 2 - 1 <= n)
+                    temp.push_back(num * 2 - 1);
+            }
+            // even part
+            for (int num : result) {
+                if (num * 2 <= n)
+                    temp.push_back(num * 2);
+            }
+            result = temp;
+        }
+        
+        return result;
+    }
+};
+
