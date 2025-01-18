8d5076318c9aafb9f093c54d586dd1d68bf03b41
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-01-18 09:29:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17 MB (39.88%)

--- 1036-rotting-oranges/solution.cpp
+++ 1036-rotting-oranges/solution.cpp
@@ -3,41 +3,43 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        int visi[n][m];
-        queue<pair<pair<int,int>,int>>q;
+    vector<vector<int>>vis(n,vector<int>(m));
+          queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                   q.push({{i,j},0});
-                   visi[i][j]=2;
+                    q.push({{i,j},0});
+                    vis[i][j]=2;
                 }
                 else{
-                    visi[i][j]=0;
+                    vis[i][j]=0;
                 }
-                
             }
         }
+
         int time=0;
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
+        int delr[]={-1,0,1,0};
+        int delc[]={0,-1,0,1};
         while(!q.empty()){
             int r=q.front().first.first;
             int c=q.front().first.second;
             int t=q.front().second;
+             q.pop();
             time=max(time,t);
-            q.pop();
             for(int i=0;i<4;i++){
-                   int nr=r+drow[i];
-                   int nc=c+dcol[i];
-                   if(nr>=0 && nr<n && nc>=0 && nc<m && visi[nr][nc]==0 && grid[nr][nc]==1){
+                int nr=r+delr[i];
+                int nc=c+delc[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0 && grid[nr][nc]==1){
                     q.push({{nr,nc},t+1});
-                    visi[nr][nc]=2;
-                   }
+                    vis[nr][nc]=2;
+                }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(visi[i][j]!=2 && grid[i][j]==1)return -1;
+                if(vis[i][j]==0 && grid[i][j]==1){
+                    return -1;
+                }
             }
         }
         return time;
