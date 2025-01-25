151a019f86ac2ea8a425a7e8197c4630d1807d75
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-01-25 13:13:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 71.9 MB (14.76%)

--- 0053-maximum-subarray/solution.cpp
+++ 0053-maximum-subarray/solution.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
 
-    int maxSubArray(vector<int>& nums) {
-        int sum=0;
-        int maxi=INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            maxi=max(sum,maxi);
-            if(sum<0)sum=0;
-        }
-        return maxi;
+    int maxSubArray(vector<int>& b) {
+           int T = INT_MIN; // Initialize T to 0, which will store the maximum sum subarray
+    int prv = 0; // Initialize prv to 0, which will store the current maximum sum subarray ending at b[i]
+ 
+    for (int i = 0; i < b.size(); ++i) {
+        int current = max(prv + b[i], b[i]);
+        //current = max(current, 0);
+        prv = current;
+        T = max(T, current);
+    }
+ 
+    return T;
     }
 };
