8050d90c370e9fcedae1023511df7d4cdbfc8c5a
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-01-29 18:43:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.7 MB (46.95%)

--- 0198-house-robber/solution.cpp
+++ 0198-house-robber/solution.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-int fun(int idx,vector<int>&nums,vector<int>&dp){
-    if(idx==0)return  nums[idx];
-    if(idx<0)return 0;
-    if(dp[idx]!=-1)return dp[idx]; 
-    int pick=nums[idx]+fun(idx-2,nums,dp);
-    int npick=0+fun(idx-1,nums,dp);
-    return dp[idx]= max(pick,npick);
+int fun(int i,vector<int>&dp,vector<int>&nums){
+    int n=nums.size();
+    if(i==0)return nums[i];
+    if(i<0)return 0;
+    if(dp[i]!=-1)return dp[i];
+
+    int ntake=fun(i-1,dp,nums);
+  
+   
+       int  take=nums[i]+fun(i-2,dp,nums);
+    
+    return dp[i]=max(take,ntake);
 }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-       return  fun(n-1,nums,dp);
+        vector<int>dp(n+1,-1);
+        return fun(n-1,dp,nums);
     }
 };
