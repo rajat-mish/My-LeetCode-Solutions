853e4dc1949abb4281e917670455389056e2aead
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-02-03 16:10:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.8 MB (72.59%)

--- 0063-unique-paths-ii/solution.cpp
+++ 0063-unique-paths-ii/solution.cpp
@@ -1,49 +1,21 @@
 class Solution {
 public:
+int fun(int i,int j,vector<vector<int>>&arr,vector<vector<int>>&dp){
+         int n=arr.size();
+        int m=arr[0].size();
+       
+    if(i==n-1 && j==m-1)return 1;
+    if(i>=n || i<0 || j>=m || j<0)return 0;
+     if(dp[i][j]!=-1)return dp[i][j];
+    if(arr[i][j]==1)return 0;
+    return dp[i][j]= fun(i+1,j,arr,dp)+fun(i,j+1,arr,dp);
+}
     int uniquePathsWithObstacles(vector<vector<int>>& arr) {
-        int n = arr.size();
-        int m = arr[0].size();
-        
-        // Declare dp vector of vectors
-        vector<vector<int>> dp(n, vector<int>(m, 0));
-        
-        // If the starting point is an obstacle, no paths are possible
-        if (arr[0][0] == 1) return 0;
+        int n=arr.size();
+        int m=arr[0].size();
+        if(arr[0][0]==1 ||arr[n-1][m-1]==1)return 0;
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return fun(0,0,arr,dp);
 
-        // Initialize the first cell
-        dp[0][0] = 1;
-
-        // Initialize the first row
-        for (int i = 1; i < m; ++i) {
-            if (arr[0][i] == 1) {
-                dp[0][i] = 0;
-            } else {
-                dp[0][i] = dp[0][i-1];
-            }
-        }
-
-        // Initialize the first column
-        for (int i = 1; i < n; ++i) {
-            if (arr[i][0] == 1) {
-                dp[i][0] = 0;
-            } else {
-                dp[i][0] = dp[i-1][0];
-            }
-        }
-
-        // Fill the rest of the dp table
-        for (int i = 1; i < n; ++i) {
-            for (int j = 1; j < m; ++j) {
-                if (arr[i][j] == 1) {
-                    dp[i][j] = 0;  // If there's an obstacle, no paths here
-                } else {
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];  // Sum of ways from top and left
-                }
-            }
-        }
-
-        // Return the number of unique paths to the bottom-right corner
-        return dp[n-1][m-1];
     }
 };
-
