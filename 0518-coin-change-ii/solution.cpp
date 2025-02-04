fcba727f0917530a962e41fc47fe3fbd2346f30b
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-02-04 17:58:39 +0000

    Sync LeetCode submission Runtime - 16 ms (77.30%), Memory - 28.3 MB (69.00%)

--- 0518-coin-change-ii/solution.cpp
+++ 0518-coin-change-ii/solution.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-vector<int>v;
-  vector<vector<int>> dp;
-int fun(int x,int startindex){
-    if(x==0)return 1;
-    if(x<0)return 0;
-    if(dp[x][startindex]!=-1)return dp[x][startindex];
-     int count=0;
-   
-    for(int i=startindex;i<v.size();i++){
-        
-       count+=fun(x-v[i],i);
-      
+int fun(int i,int t,vector<int>&coins,vector<vector<int>>&dp){
+    if(i==0){
+        if(t%coins[i]==0)return 1;
+        return 0;
     }
-    dp[x][startindex]=count;
-    
-    return count;
 
+    if(dp[i][t]!=-1)return dp[i][t];
+    int ntake=fun(i-1,t,coins,dp);
+    int take=0;
+
+    if(coins[i]<=t){
+        take=fun(i,t-coins[i],coins,dp);
+    }
+    return dp[i][t]=take+ntake;
 }
-
     int change(int amount, vector<int>& coins) {
-        dp.clear();
-         dp.resize(amount + 1, vector<int>(coins.size(), -1)); 
-        v=coins;
-        return fun(amount,0);
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return fun(n-1,amount,coins,dp);
     }
 };
-
-
