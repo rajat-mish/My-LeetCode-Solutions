e419958ed3e495de15d81d2f065f57311ef720de
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-02-09 07:53:26 +0000

    Sync LeetCode submission Runtime - 3 ms (32.11%), Memory - 16.3 MB (36.06%)

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
 int fun(int i,int buy,vector<int>&prices,vector<vector<int>>&dp){
-    if(i>=prices.size()){
-        return 0;
-    }
+    if(i>=prices.size())return 0;
     if(dp[i][buy]!=-1)return dp[i][buy];
     int profit=0;
     if(buy){
-      profit=max(-prices[i]+fun(i+1,0,prices,dp),fun(i+1,1,prices,dp));
+        profit=max(-prices[i]+fun(i+1,0,prices,dp),fun(i+1,1,prices,dp));
     }
     else{
-     profit=max(prices[i]+fun(i+2,1,prices,dp),fun(i+1,0,prices,dp));
+        profit=max(prices[i]+fun(i+2,1,prices,dp),fun(i+1,0,prices,dp));
     }
-    return dp[i][buy]= profit;
+    return dp[i][buy]=profit;
 }
     int maxProfit(vector<int>& prices) {
-                int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
+        vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
         return fun(0,1,prices,dp);
     }
 };
