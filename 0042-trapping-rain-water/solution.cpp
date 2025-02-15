0982ce209ac71993eab26cdb158b1f5a046e158e
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-02-15 18:54:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 25.9 MB (93.90%)

--- 0042-trapping-rain-water/solution.cpp
+++ 0042-trapping-rain-water/solution.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n=height.size();
-        int prev[n];
-        prev[0]=-1;
-        int max=height[0];
-        for(int i=1;i<n;i++){
-            prev[i]=max;
-            if(max<height[i]) max=height[i];
-        }
-        int next[n];
-        next[n-1]=-1;
-        max=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            next[i]=max;
-              if(max<height[i]) max=height[i];
-
-        }
-        for(int i=0;i<n;i++){
-            prev[i]=min(prev[i],next[i]);
-        }
-        int water=0;
-        for(int i=1;i<n-1;i++){
-            if(height[i]<prev[i]){
-                water+=prev[i]-height[i];
+    int trap(vector<int>& arr) {
+                int left=0;
+        int right=arr.size()-1;
+        int lmax=arr[left];
+        int rmax=arr[right];
+        
+        int ans=0;
+        while(left<right){
+            if(arr[right]>arr[left]){
+                lmax=max(lmax,arr[left]);
+                ans+=lmax-arr[left];
+                left++;
+            }
+            else{
+                rmax=max(rmax,arr[right]);
+                ans+=rmax-arr[right];
+                right--;
             }
         }
-        return water;
+        return ans;
     }
 };
