30faaca795a429920b77e36da6f28536a2c3e247
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-02-28 16:43:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.7 MB (52.09%)

--- 0213-house-robber-ii/solution.cpp
+++ 0213-house-robber-ii/solution.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-vector<int>dp1;
-vector<int>dp2;
-int fun1(vector<int>&v,int i){
-    if(i==v.size()-2)return v[i];
-    if(i==v.size()-3)return max(v[i],v[i+1]);
+int fun1(int i,vector<int>&nums,vector<int>&dp1){
+    if(i==0)return nums[i];
+    if(i<0)return 0;
     if(dp1[i]!=-1)return dp1[i];
-    return dp1[i]=max((v[i]+fun1(v,i+2)),0+fun1(v,i+1));
+    return dp1[i]= max(fun1(i-1,nums,dp1),nums[i]+fun1(i-2,nums,dp1));
 }
 
-int fun2(vector<int>&v,int i){
-   if(i==v.size()-1)return v[i];
-    if(i==v.size()-2)return max(v[i],v[i+1]);
+int fun2(int i,vector<int>&nums,vector<int>&dp2){
+    if(i==1)return nums[i];
+    if(i<=0)return 0;
     if(dp2[i]!=-1)return dp2[i];
-    return dp2[i]=max((v[i]+fun2(v,i+2)),0+fun2(v,i+1));
+    return dp2[i]= max(fun2(i-1,nums,dp2),nums[i]+fun2(i-2,nums,dp2));
 }
 
 
     int rob(vector<int>& nums) {
-        dp1.clear();
-        dp1.resize(105,-1);
-         dp2.clear();
-        dp2.resize(105,-1);
-        if(nums.size()==1){
-           return nums[0];
-        }
-       else return max(fun1(nums,0),fun2(nums,1));
+          int n=nums.size();
+        if(n==1)return nums[0];
+      
+        vector<int>dp1(n+1,-1);
+        vector<int>dp2(n+1,-1);
+        return max(fun1(n-2,nums,dp1),fun2(n-1,nums,dp2));
     }
 };
