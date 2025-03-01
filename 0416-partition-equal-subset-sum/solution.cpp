d4b2b8526fe016a19846fd52eed2bb03b1d545b0
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-03-01 05:35:35 +0000

    Sync LeetCode submission Runtime - 129 ms (56.86%), Memory - 100.4 MB (46.83%)

--- 0416-partition-equal-subset-sum/solution.cpp
+++ 0416-partition-equal-subset-sum/solution.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-  bool fun(int idx,int sum,int target,vector<int>&arr,vector<vector<int>>&dp){
-       if(sum==target)return true;
-     if(idx==arr.size()||sum>target){
-      
-         return false;
-     }
-   
-   
-        if(dp[idx][sum]!=-1)return dp[idx][sum];
-      int ntake=fun(idx+1,sum,target,arr,dp);
-      int take=false;
-      if(sum+arr[idx] <=target){
-          take=fun(idx+1,sum+arr[idx],target,arr,dp);
-      }
-      return dp[idx][sum]= max(take,ntake);
-      
-  }
+bool fun(int i,int target,vector<int>&nums,vector<vector<int>>&dp){
+    if(target==0)return true;
+    if(i==0){
+        return nums[i]==target;
+    }
+    if(dp[i][target]!=-1)return dp[i][target];
+    int ntake=fun(i-1,target,nums,dp);
+    int take=false;
+    if(target>=nums[i]){
+        take=fun(i-1,target-nums[i],nums,dp);
+    }
+    return dp[i][target]=take||ntake;
+}
     bool canPartition(vector<int>& nums) {
+        int n=nums.size();
+     
         int sum=0;
-        for(auto ele:nums){
-            sum+=ele;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
         }
         if(sum%2!=0)return false;
         int target=sum/2;
-          int n=nums.size();
-   vector<vector<int>> dp(n, vector<int>(target + 1, -1));
-
-        return fun(0,0,target,nums,dp);
+           vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        return fun(n-1,sum/2,nums,dp);
     }
 };
