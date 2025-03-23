306997289c7470f37720f4edfbe44baea6fdb12a
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-03-23 19:47:50 +0000

    Sync LeetCode submission Runtime - 58 ms (87.91%), Memory - 11.2 MB (47.75%)

---
+++ 1890-sum-of-beauty-of-all-substrings/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int beautySum(string s) {
+       
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+          
+             vector<int>freq(26,0);
+            for(int j=i;j<s.size();j++){
+                freq[s[j]-'a']++;
+                   int mx = 0, mn = INT_MAX;
+            for (int f : freq) {
+                if (f > 0) {
+                    mx = max(mx, f);
+                    mn = min(mn, f);
+                }
+            }
+                ans+=mx-mn;
+            }
+        }
+      return ans;
+       
+    }
+};
