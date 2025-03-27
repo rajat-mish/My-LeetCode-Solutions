4b716f0b100de77317df4c29ce0354e3c6739a1d
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-03-27 20:21:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.4 MB (93.36%)

--- 0050-powx-n/solution.cpp
+++ 0050-powx-n/solution.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    double myPow(double x, int n) {double res=1.00000;
-      if(n==0){
-          return 1;
-      }
-      long long a=n;
-      if(n<0){
-          x=1/x;
-          a=-a;
-      }
-      if(a%2!=0){
-          return x*myPow(x*x,a/2);
-      }else{
-          return myPow(x*x,a/2);
-      }
+double fun(double x,int n){
+    if(n==0)return 1;
+    if(n==1)return x;
+    if(n%2!=0)return x*fun(x*x,n/2);
+    return fun(x*x,n/2);
+}
+
+    double myPow(double x, int n) {
+         long long a=n;
+         if(n<0){
+        
+         a = -1LL * n; 
+            return 1/fun(x,a);
+         }
+         return fun(x,n);
     }
 };
