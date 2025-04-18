815bc68ab099512520b9404bf1e09f46c1c14c81
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-04-18 09:39:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (82.48%)

--- 0231-power-of-two/solution.cpp
+++ 0231-power-of-two/solution.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int a) {
-      if(a==0)return false;
-    if(a==1){
-        return true;
-    }
-    if(a%2==0){
-        return isPowerOfTwo(a/2);
-    }
-    return false;
-
+    bool isPowerOfTwo(int n) {
+        if(n<=0)return false;
+        if((n&(n-1))==0)return true;
+        return false;
     }
 };
