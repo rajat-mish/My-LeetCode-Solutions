50343c598869797770be942c8488b4b6fe1def56
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-05-30 17:05:57 +0000

    Sync LeetCode submission Runtime - 3 ms (26.03%), Memory - 17.2 MB (45.63%)

--- 0102-binary-tree-level-order-traversal/solution.cpp
+++ 0102-binary-tree-level-order-traversal/solution.cpp
@@ -15,17 +15,18 @@ int levels(TreeNode*root){
     if(root==NULL)return 0;
     return 1+max(levels(root->left),levels(root->right));
 }
-void printNth(TreeNode*root,int cur,int n,vector<int>&v){
-  if(root==NULL)return;
-  if(cur==n){
-    v.push_back(root->val);
-    return ;
-  }
-  printNth(root->left,cur+1,n,v);
-  printNth(root->right,cur+1,n,v);
 
+void printNth(TreeNode*root,int cur,int n,vector<int>&v){
+    if(root==NULL)return ;
+    if(cur==n){
+        v.push_back(root->val);
+        return;
+    }
+    printNth(root->left,cur+1,n,v);
+    printNth(root->right,cur+1,n,v);
 }
-void lev(TreeNode*root,vector<vector<int>>&ans){
+
+void fun(TreeNode*root,vector<vector<int>>&ans){
     int n=levels(root);
     for(int i=1;i<=n;i++){
         vector<int>v;
@@ -33,9 +34,11 @@ void lev(TreeNode*root,vector<vector<int>>&ans){
         ans.push_back(v);
     }
 }
+
+
     vector<vector<int>> levelOrder(TreeNode* root) {
         vector<vector<int>>ans;
-        lev(root,ans);
+        fun(root,ans);
         return ans;
     }
 };
