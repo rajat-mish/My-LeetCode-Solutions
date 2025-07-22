f67a29d1560daff293943fe52828c8a53dab70ab
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-07-22 18:16:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.4 MB (30.71%)

--- 0062-unique-paths/solution.cpp
+++ 0062-unique-paths/solution.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-// tabulation
+int fun(int i,int j,int m,int n,vector<vector<int>>&dp){
+    if(i==m-1 && j==n-1)return 1;
+    if(dp[i][j]!=-1)return dp[i][j];
+
+    if(i>=m || i<0 || j>=n || j<0)return 0;
+
+    return dp[i][j]= fun(i+1,j,m,n,dp)+fun(i,j+1,m,n,dp);
+}
     int uniquePaths(int m, int n) {
-       int dp[m][n];
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(i==0 || j==0){
-                dp[i][j]=1;
-            }
-            else{
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
-       }
-       return dp[m-1][n-1]; 
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return fun(0,0,m,n,dp);
     }
 };
