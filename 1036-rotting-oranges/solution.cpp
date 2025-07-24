71d566a7c47b0cb7c9dfae9667fba8b1dd9fc86a
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-07-24 16:52:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.1 MB (28.98%)

--- 1036-rotting-oranges/solution.cpp
+++ 1036-rotting-oranges/solution.cpp
@@ -3,45 +3,46 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-    vector<vector<int>>vis(n,vector<int>(m));
-          queue<pair<pair<int,int>,int>>q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+         vector<vector<int>>vis(n,vector<int>(m));
+        queue<pair<int,pair<int,int>>>q;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j},0});
+                    q.push({0,{i,j}});
                     vis[i][j]=2;
                 }
-                else{
-                    vis[i][j]=0;
-                }
+                else vis[i][j]=0;
             }
         }
-
-        int time=0;
-        int delr[]={-1,0,1,0};
-        int delc[]={0,-1,0,1};
+    
+        int ans=0;
+       
         while(!q.empty()){
-            int r=q.front().first.first;
-            int c=q.front().first.second;
-            int t=q.front().second;
-             q.pop();
-            time=max(time,t);
+            int time=q.front().first;
+            int row=q.front().second.first;
+            int col=q.front().second.second;
+
+            q.pop();
+            ans=max(ans,time);
+            int delr[]={0,-1,0,1};
+            int delc[]={-1,0,1,0};
             for(int i=0;i<4;i++){
-                int nr=r+delr[i];
-                int nc=c+delc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0 && grid[nr][nc]==1){
-                    q.push({{nr,nc},t+1});
-                    vis[nr][nc]=2;
+                int nr=row+delr[i];
+                int nc=col+delc[i];
+
+                if(nr<n && nr>=0 && nc<m && nc>=0){
+                    if(grid[nr][nc]==1 && vis[nr][nc]==0){
+                        q.push({time+1,{nr,nc}});
+                        vis[nr][nc]=2;
+                    }
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]==0 && grid[i][j]==1){
-                    return -1;
-                }
+                if(vis[i][j]==0 && grid[i][j]==1)return -1;
             }
         }
-        return time;
+        return ans;
     }
 };
