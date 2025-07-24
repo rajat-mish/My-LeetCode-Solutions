453053183e0862e9d14d1bd28e8f1b6feee5699a
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-07-24 17:15:02 +0000

    Sync LeetCode submission Runtime - 9 ms (27.07%), Memory - 20 MB (32.58%)

--- 0207-course-schedule/solution.cpp
+++ 0207-course-schedule/solution.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
-    bool canFinish(int V, vector<vector<int>>& prereq) {
-        vector<vector<int>> adj(V);
-
-          for(auto ele:prereq){
-            adj[ele[0]].push_back(ele[1]);
-          }
-          
+    bool canFinish(int numCourses, vector<vector<int>>& pre) {
         
-        
-        vector<int>indegree(V,0);
-            for(int i=0;i<V;i++){
-                      for(auto ele:adj[i]){
-            indegree[ele]++;
+        int n=numCourses;
+        vector<vector<int>>adj(n);
+     for(auto ele:pre){
+        adj[ele[0]].push_back(ele[1]);
+     }
+      
+       vector<int>indeg(n,0);
+       vector<int>topo;
+       for(int i=0;i<n;i++){
+        for(auto ele:adj[i])indeg[ele]++;
+       }
+       queue<int>q;
+       for(int i=0;i<n;i++){
+        if(indeg[i]==0)q.push(i);
+       }
+       while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        topo.push_back(node);
+        for(auto ele:adj[node]){
+            indeg[ele]--;
+            if(indeg[ele]==0)q.push(ele);
         }
-            }
-            
-            queue<int>q;
-            vector<int>topo;
-            for(int i=0;i<V;i++){
-                if(indegree[i]==0){
-                    q.push(i);
-                }
-            }
-            while(!q.empty()){
-                int node=q.front();
-                q.pop();
-                topo.push_back(node);
-                for(auto ele:adj[node]){
-                    indegree[ele]--;
-                    if(indegree[ele]==0){
-                        q.push(ele);
-                    }
-                }
-                
-            }
-          if(topo.size()==V){
-              return true;
-          }
-          return false;
+       }
+
+       if(topo.size()==n)return true;
+       return false;
+
     }
 };
