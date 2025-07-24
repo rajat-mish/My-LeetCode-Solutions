ff164bee6ec8032e840174c283aa7db0301e960b
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-07-24 10:41:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.3 MB (68.96%)

--- 0062-unique-paths/solution.cpp
+++ 0062-unique-paths/solution.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-int fun(int i,int j,int m,int n,vector<vector<int>>&dp){
-    if(i==m-1 && j==n-1)return 1;
-    if(dp[i][j]!=-1)return dp[i][j];
-
+vector<vector<int>>dp;
+int fun(int i,int j,int m,int n){
     if(i>=m || i<0 || j>=n || j<0)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+    if(i==m-1 && j==n-1)return 1;
 
-    return dp[i][j]= fun(i+1,j,m,n,dp)+fun(i,j+1,m,n,dp);
+    return dp[i][j]= fun(i+1,j,m,n)+fun(i,j+1,m,n);
 }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return fun(0,0,m,n,dp);
+        dp.resize(m,vector<int>(n,-1));
+        return fun(0,0,m,n);
     }
 };
