0b485e3de32538eb7831db4cc32f66a895657459
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-07-28 17:18:57 +0000

    Sync LeetCode submission Runtime - 519 ms (6.34%), Memory - 274.2 MB (5.20%)

--- 0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ 0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
+vector<vector<vector<int>>>dp;
+int fun(int i,int buy,int cnt,vector<int>&prices){
+    if(i>=prices.size() || cnt==1)return 0;
+      int profit=0;
+      if(dp[i][buy][cnt]!=-1)return dp[i][buy][cnt];
+    if(buy){
+        profit= max(-prices[i]+fun(i+1,0,0,prices),fun(i+1,1,0,prices));
+    }
+    else{
+        profit= max(prices[i]+fun(i+1,1,1,prices),fun(i+1,0,0,prices));
+    }
+    return dp[i][buy][cnt]= profit;
+}
     int maxProfit(vector<int>& prices) {
-        int mini=prices[0];
-        int ans=0;
-        for(int i=1;i<prices.size();i++){
-           int cost=prices[i]-mini;
-            ans=max(ans,cost);
-            mini=min(mini,prices[i]);
-        }
-       
-        return ans;
+        dp.resize(prices.size()+1,vector<vector<int>>(2,vector<int>(2,-1)));
+        return fun(0,1,0,prices);
     }
 };
