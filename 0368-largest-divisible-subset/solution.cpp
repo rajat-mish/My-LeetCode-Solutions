20af451503bf9e24d8a62dd32ea95d448fa9d383
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-07-30 09:08:04 +0000

    Sync LeetCode submission Runtime - 2651 ms (5.00%), Memory - 550.1 MB (5.42%)

--- 0368-largest-divisible-subset/solution.cpp
+++ 0368-largest-divisible-subset/solution.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-vector<int> fun(int i,int prev,vector<int>&nums,vector<vector<vector<int>>>&dp){
-    if(i==nums.size())return {};
-
-    if(!dp[i][prev+1].empty())return dp[i][prev+1];
-    vector<int>exclude=fun(i+1,prev,nums,dp);
-    vector<int>include;
-
-    if(prev==-1|| nums[i]%nums[prev]==0){
-        include=fun(i+1,i,nums,dp);
-        include.push_back(nums[i]);
+vector<vector<vector<int>>>dp;
+vector<int>fun(int i,int prev,vector<int>&nums){
+    if(i>=nums.size())return {};
+     if(!dp[i][prev+1].empty())return dp[i][prev+1];
+    vector<int>exc=fun(i+1,prev,nums);
+    vector<int>incl;
+    if(prev==-1 || (nums[i]%nums[prev]==0 )){
+         incl=fun(i+1,i,nums);
+        incl.push_back(nums[i]);
+        //ans= max(ans,fun(i+1,i,nums));
+       
     }
-    return dp[i][prev+1]=(include.size()>exclude.size())?include:exclude;
+    //ans.pop_back();
+    return dp[i][prev+1]= (incl.size()>exc.size())?incl:exc;
 }
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int n=nums.size();
-      vector<vector<vector<int>>>dp(n,vector<vector<int>>(n+1));
-        vector<int>ans= fun(0,-1,nums,dp);
-
-        return ans;
-
+        dp.resize(n+1,vector<vector<int>>(n+1));
+        return fun(0,-1,nums);
     }
 };
-
-
