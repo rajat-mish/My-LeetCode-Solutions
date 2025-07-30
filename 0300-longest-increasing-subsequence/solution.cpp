e4d5ae29517154dff1e0684efd94956a6c69e1a0
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-07-30 08:03:50 +0000

    Sync LeetCode submission Runtime - 530 ms (21.52%), Memory - 296.6 MB (6.71%)

--- 0300-longest-increasing-subsequence/solution.cpp
+++ 0300-longest-increasing-subsequence/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-int fun(vector<int>&v,int i,int pre,vector<vector<int>>&dp){
-     if(i==v.size())return 0;
-     if(dp[i][pre+1]!=-1)return dp[i][pre+1];
-    int len=fun(v,i+1,pre,dp);
-   
-    if(pre==-1||v[pre]<v[i]){
-       
-        len=max(len,1+fun(v,i+1,i,dp));
+vector<vector<int>>dp;
+int fun(int i,int prev,vector<int>&nums){
+    if(i>=nums.size())return 0;
+    if(dp[i][prev+1]!=-1)return dp[i][prev+1];
+    int len=fun(i+1,prev,nums);
+
+    if (prev==-1 || nums[i]>nums[prev]){
+        len=max(len,1+fun(i+1,i,nums));
     }
-   
-    return dp[i][pre+1]= len;
-   
+    return dp[i][prev+1]= len;
 }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n,-1));
-        return fun(nums,0,-1,dp);
+        dp.resize(n+1,vector<int>(n+1,-1));
+        return fun(0,-1,nums);
     }
 };
