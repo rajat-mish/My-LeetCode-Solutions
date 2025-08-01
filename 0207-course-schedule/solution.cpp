e0ac3a4ed25c32ab2acf17469dc10bd5745430f6
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-08-01 16:40:18 +0000

    Sync LeetCode submission Runtime - 10 ms (23.89%), Memory - 19.5 MB (44.37%)

--- 0207-course-schedule/solution.cpp
+++ 0207-course-schedule/solution.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& pre) {
-        
-        int n=numCourses;
-        vector<vector<int>>adj(n);
-     for(auto ele:pre){
-        adj[ele[0]].push_back(ele[1]);
-     }
-      
-       vector<int>indeg(n,0);
-       vector<int>topo;
-       for(int i=0;i<n;i++){
-        for(auto ele:adj[i])indeg[ele]++;
-       }
-       queue<int>q;
-       for(int i=0;i<n;i++){
-        if(indeg[i]==0)q.push(i);
-       }
-       while(!q.empty()){
+    bool canFinish(int V, vector<vector<int>>& pre) {
+        vector<int>adj[V];
+        for(auto ele:pre){
+            adj[ele[0]].push_back(ele[1]);
+        }
+        vector<int>ind(V,0);
+        for(int i=0;i<V;i++){
+            for(auto ele:adj[i]){
+                ind[ele]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(ind[i]==0)q.push(i);
+        }
+        vector<int>ans;
+        while(!q.empty()){
         int node=q.front();
         q.pop();
-        topo.push_back(node);
+        ans.push_back(node);
         for(auto ele:adj[node]){
-            indeg[ele]--;
-            if(indeg[ele]==0)q.push(ele);
+            ind[ele]--;
+            if(ind[ele]==0)q.push(ele);
         }
-       }
-
-       if(topo.size()==n)return true;
-       return false;
-
+        }
+        if(ans.size()==V)return true;
+        return false;
     }
 };
