df2091a3b4af1cf5789be2a6ae06e8ce29cb783e
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-08-01 04:33:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.9 MB (28.04%)

--- 0046-permutations/solution.cpp
+++ 0046-permutations/solution.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
+vector<vector<int>>ans;
+void fun(int i,vector<int>&nums){
+   if(i>=nums.size()){
+    ans.push_back(nums);
+    return ;
+   }
+
+   for(int idx=i;idx<nums.size();idx++){
+    swap(nums[i],nums[idx]);
+     fun(i+1,nums);
+    swap(nums[i],nums[idx]);
+   
+
+   }
+}
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans;
-        
-        sort(nums.begin(),nums.end());
-        do{
-         ans.push_back(nums);
-        }while(next_permutation(nums.begin(),nums.end()));
+        fun(0,nums);
         return ans;
     }
 };
