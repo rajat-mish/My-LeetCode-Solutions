a8f82408f69c565fe82872fee14471e789d513ad
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-08-02 07:27:08 +0000

    Sync LeetCode submission Runtime - 238 ms (5.06%), Memory - 120 MB (5.14%)

--- 0128-longest-consecutive-sequence/solution.cpp
+++ 0128-longest-consecutive-sequence/solution.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int longestConsecutive(vector<int>& arr) {
-        if(arr.size()==0)return 0;
-     vector<int>ans;
+    int longestConsecutive(vector<int>& nums) {
+        set<int>st;
+        unordered_map<int,int>mp;
+        for(auto ele:nums){
+            st.insert(ele);
+            mp[ele]++;
+        }
         int count=1;
-        sort(arr.begin(),arr.end());
-        
-        for(int i=0;i<arr.size()-1;i++){
-            if((arr[i+1]-arr[i])==1){
+        int ans=0;
+        for(auto ele:st){
+            if(mp.find(ele+1)!=mp.end()){
                 count++;
             }
-            else if((arr[i+1]-arr[i])==0){
-                continue;
-            }
             else{
-                ans.push_back(count);
+                ans=max(ans,count);
                 count=1;
             }
         }
-        ans.push_back(count);
-        return *max_element(ans.begin(),ans.end());
+        return ans;
         
     }
 };
