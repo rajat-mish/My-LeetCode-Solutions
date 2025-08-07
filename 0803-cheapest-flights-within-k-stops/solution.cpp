09165e5eb96d9963fb98e55fc0fdd3ea609e276e
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-08-07 13:57:26 +0000

    Sync LeetCode submission Runtime - 3 ms (61.35%), Memory - 18.2 MB (43.63%)

--- 0803-cheapest-flights-within-k-stops/solution.cpp
+++ 0803-cheapest-flights-within-k-stops/solution.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        // there is no need of priority queue , normal queue se kaam chal jayega
-      
-                vector<pair<int,int>>adj[n];
+        vector<pair<int,int>>adj[n];
         for(auto ele:flights){
             adj[ele[0]].push_back({ele[1],ele[2]});
         }
-        queue<pair<int,pair<int,int>>>q;  // stops,node,cost
+vector<int>dist(n,INT_MAX);
+        queue<pair<int,pair<int,int>>>q;
         q.push({0,{src,0}});
-        vector<int>dist(n,1e9);
         dist[src]=0;
-        while( !q.empty()){
-            int stops=q.front().first;
+        int ans=INT_MAX;
+        while(!q.empty()){
+            int price=q.front().first;
             int node=q.front().second.first;
-            int cost=q.front().second.second;
+            int stop=q.front().second.second;
             q.pop();
-            if(stops>k)continue;
-            for(auto ele:adj[node]){
-                if(cost+ele.second <dist[ele.first] && stops<=k){
-                    dist[ele.first]=cost+ele.second;
-                    q.push({stops+1,{ele.first,cost+ele.second}});
-                }
+             
+           if(stop>k)continue;
+           for(auto ele:adj[node]){
+            if(price+ele.second<dist[ele.first]){
+                dist[ele.first]=price+ele.second;
+            if(stop<=k) q.push({price+ele.second,{ele.first,stop+1}});
             }
+           }
+
         }
-        if(dist[dst]==1e9)return -1;
+        if(dist[dst]==INT_MAX)return -1;
         return dist[dst];
+
     }
 };
