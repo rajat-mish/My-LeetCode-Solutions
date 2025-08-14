d635601d594b1abb729c73b4eeadf1da480f624d
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-08-14 13:38:29 +0000

    Sync LeetCode submission Runtime - 77 ms (12.47%), Memory - 57 MB (5.05%)

--- 0721-accounts-merge/solution.cpp
+++ 0721-accounts-merge/solution.cpp
@@ -1,88 +1,49 @@
-class DisjointSet {
-public:
-    vector<int> parent, rank, size;
-
-    DisjointSet(int n) {
-        parent.resize(n);
-        rank.resize(n, 0);
-        size.resize(n, 1); // size of each component initially is 1
-        for(int i = 0; i < n; i++) parent[i] = i;
-    }
-
-    int findParent(int node) {
-        if(node == parent[node]) return node;
-        return parent[node] = findParent(parent[node]);
-    }
-
-    void unionByRank(int u, int v) {
-        int pu = findParent(u);
-        int pv = findParent(v);
-        if(pu == pv) return;
-
-        if(rank[pu] < rank[pv]) {
-            parent[pu] = pv;
-        }
-        else if(rank[pv] < rank[pu]) {
-            parent[pv] = pu;
-        }
-        else {
-            parent[pv] = pu;
-            rank[pu]++;
-        }
-    }
-
-    void unionBySize(int u, int v) {
-        int pu = findParent(u);
-        int pv = findParent(v);
-        if(pu == pv) return;
-
-        if(size[pu] < size[pv]) {
-            parent[pu] = pv;
-            size[pv] += size[pu];
-        }
-        else {
-            parent[pv] = pu;
-            size[pu] += size[pv];
-        }
-    }
-};
-
+#include <bits/stdc++.h>
+using namespace std;
 
 class Solution {
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        int n=accounts.size();
-        DisjointSet ds(n);
-        unordered_map<string,int>mp;
-        for(int i=0;i<n;i++){
-            for(int j=1;j<accounts[i].size();j++){ // don't include first element bcz its name
-                string mail=accounts[i][j];
-                if(mp.find(mail)==mp.end()){
-                    mp[mail]=i;
-                }
-                else{
-                    ds.unionByRank(i,mp[mail]);
-                }
+    unordered_map<string, vector<string>> graph; // email -> connected emails
+    unordered_map<string, string> emailToName;   // email -> name
+    unordered_set<string> visited;
+
+    void dfs(string email, vector<string> &emails) {
+        visited.insert(email);
+        emails.push_back(email);
+
+        for (auto &nei : graph[email]) {
+            if (!visited.count(nei)) {
+                dfs(nei, emails);
             }
         }
-        vector<string>mergemail[n];
-        for(auto ele:mp){
-            string mail=ele.first;
-            int node=ds.findParent(ele.second);
-            mergemail[node].push_back(mail);
+    }
+
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        // Build graph and email-to-name map
+        for (auto &acc : accounts) {
+            string name = acc[0];
+            for (int i = 1; i < acc.size(); i++) {
+                emailToName[acc[i]] = name;
+                if (i == 1) continue;
+                graph[acc[1]].push_back(acc[i]);
+                graph[acc[i]].push_back(acc[1]);
+            }
         }
 
-        vector<vector<string>>ans;
-        for(int i=0;i<n;i++){
-            if(mergemail[i].size()==0)continue;
-            sort(mergemail[i].begin(),mergemail[i].end());
-            vector<string>temp;
-            temp.push_back(accounts[i][0]);
-            for(auto ele:mergemail[i]){
-                temp.push_back(ele);
+        vector<vector<string>> result;
+
+        // DFS for each email
+        for (auto &p : emailToName) {
+            string email = p.first;
+            if (!visited.count(email)) {
+                vector<string> emails;
+                dfs(email, emails);
+                sort(emails.begin(), emails.end());
+                emails.insert(emails.begin(), emailToName[email]);
+                result.push_back(emails);
             }
-            ans.push_back(temp);
         }
-        return ans;
+        return result;
     }
 };
+
