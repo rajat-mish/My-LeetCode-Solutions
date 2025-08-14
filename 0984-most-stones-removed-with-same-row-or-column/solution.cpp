e8ae4668983e0dac27229a4107d75d46c55cdb1a
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-08-14 13:19:55 +0000

    Sync LeetCode submission Runtime - 43 ms (32.57%), Memory - 20.8 MB (64.57%)

--- 0984-most-stones-removed-with-same-row-or-column/solution.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/solution.cpp
@@ -1,62 +1,30 @@
-class DisjointSet {
-public:
-    vector<int> parent, rank;
-    
-    DisjointSet(int n) {
-        parent.resize(n);
-        rank.resize(n, 0);
-        for(int i = 0; i < n; i++)
-            parent[i] = i;
-    }
-    
-    int findParent(int node) {
-        if(parent[node] != node)
-            parent[node] = findParent(parent[node]);
-        return parent[node];
-    }
-    
-    void unionByRank(int u, int v) {
-        int parent_u = findParent(u);
-        int parent_v = findParent(v);
-        
-        if(parent_u == parent_v) return;
-        
-        if(rank[parent_u] < rank[parent_v]) {
-            parent[parent_u] = parent_v;
-        }
-        else if(rank[parent_v] < rank[parent_u]) {
-            parent[parent_v] = parent_u;
-        }
-        else {
-            parent[parent_v] = parent_u;
-            rank[parent_u]++;
-        }
-    }
-};
-
-
-
-
-
 class Solution {
 public:
-const int shift = 10001;
-
+void dfs(int node,vector<int>&vis,vector<vector<int>>&graph){
+    vis[node]=1;
+    for(auto ele:graph[node]){
+        if(!vis[ele])dfs(ele,vis,graph);
+    }
+}
     int removeStones(vector<vector<int>>& stones) {
-         DisjointSet ds(20005);
-         unordered_set<int>usedNodes;
-        for(auto ele:stones){
-            int u=ele[0];
-            int v=ele[1]+shift;
-            ds.unionByRank(u,v);
-            usedNodes.insert(u);
-            usedNodes.insert(v);
-              
+        int n=stones.size();
+        vector<vector<int>>graph(n);
+       for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1]){
+                graph[i].push_back(j);
+                graph[j].push_back(i);
+            }
         }
-        int components=0;
-        for(auto ele:usedNodes){
-            if(ds.findParent(ele)==ele)components++;
+       }
+       int cnt=0;
+       vector<int>vis(n,0);
+       for(int i=0;i<n;i++){
+        if(!vis[i]){
+            dfs(i,vis,graph);
+            cnt++;
         }
-        return stones.size()-components;
+       }
+       return n-cnt;
     }
 };
