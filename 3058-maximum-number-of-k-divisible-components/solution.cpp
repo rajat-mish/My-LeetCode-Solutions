0cd01f73c64a971e626dbe9ac54a2989e08fcac4
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-11-28 17:00:40 +0000

    Sync LeetCode submission Runtime - 111 ms (58.99%), Memory - 173.2 MB (91.01%)

---
+++ 3058-maximum-number-of-k-divisible-components/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int cnt;
+    long long dfs(int node, int parent, vector<vector<int>>& adj, vector<int>& values, int k) {
+        long long sum = values[node];
+
+        for (auto child : adj[node]) {
+            if (child == parent) continue;
+            sum+= dfs(child, node, adj, values, k);
+        }
+
+        if (sum%k == 0) {
+            cnt++;
+            return 0;
+        }
+        return sum;
+    }
+
+    int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
+        vector<vector<int>> adj(n);
+        for (auto &e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
+        cnt = 0;
+        dfs(0,-1,adj,values,k);
+        return cnt;
+    }
+};
+
