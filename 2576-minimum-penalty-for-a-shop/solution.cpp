95772f1c321113bd8cb9b961c46ae1f93f3483c3
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2025-12-26 12:13:33 +0000

    Sync LeetCode submission Runtime - 8 ms (55.54%), Memory - 21.8 MB (27.73%)

--- 2576-minimum-penalty-for-a-shop/solution.cpp
+++ 2576-minimum-penalty-for-a-shop/solution.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    int bestClosingTime(string customers) {
-        int n=customers.size();
-        int pre[n+1];
-        int suf[n+1];
-        pre[0]=0;
-        for(int i=0;i<n;i++){int count=0;
-            if(customers[i]=='N')count++;
-            pre[i+1]=pre[i]+count;
+    int bestClosingTime(string cus) {
+        int n=cus.size();
+        vector<int>pre(n+1),suf(n+1);
+    pre[0]=0;
+        for(int i=1;i<=cus.size();i++){
+           
+                pre[i]=pre[i-1]+(cus[i-1]=='N');
+            
+           
         }
+
         suf[n]=0;
-        for(int i=n-1;i>=0;i--){int c=0;
-            if(customers[i]=='Y')c++;
-            suf[i]=suf[i+1]+c;
+        for(int i=n-1;i>=0;i--){
+          
+                suf[i]=suf[i+1]+(cus[i]=='Y');
+           
         }
+        int ans=INT_MAX;
+        int idx=0;
         for(int i=0;i<=n;i++){
-            pre[i]+=suf[i];
+          if(ans>suf[i]+pre[i]){
+            ans=suf[i]+pre[i];
+            idx=i;
+          }
         }
-        int min=pre[0];
-        
-         for(int i=0;i<=n;i++){
-            if(pre[i]<min){
-                min=pre[i];
-                
-            }
-        }
-         for(int i=0;i<=n;i++){
-            if(pre[i]==min)return i;
-        }
-        return n;
+      return idx;
+
     }
 };
