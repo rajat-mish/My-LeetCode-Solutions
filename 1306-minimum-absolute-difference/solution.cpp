b4ae69e417f2169213d32e6f43843916add0b082
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2026-01-26 07:48:50 +0000

    Sync LeetCode submission Runtime - 16 ms (48.74%), Memory - 36.8 MB (18.33%)

--- 1306-minimum-absolute-difference/solution.cpp
+++ 1306-minimum-absolute-difference/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-         sort(arr.begin(),arr.end());
+            sort(arr.begin(),arr.end());
           vector<vector<int>>ans;
          
     int minD=INT_MAX;
@@ -20,22 +20,5 @@ public:
             
           }
           return ans;
-
-
-        //  std::sort(arr.begin(), arr.end());
-        // std::vector<std::vector<int>> ans;
-        // int minDiff = INT_MAX;
-
-        // for (int i = 1; i < arr.size(); i++) {
-        //     minDiff = std::min(minDiff, arr[i] - arr[i - 1]);
-        // }
-
-        // for (int i = 1; i < arr.size(); i++) {
-        //     if (arr[i] - arr[i - 1] == minDiff) {
-        //         ans.push_back({arr[i - 1], arr[i]});
-        //     }
-        // }
-
-        // return ans;
     }
 };
