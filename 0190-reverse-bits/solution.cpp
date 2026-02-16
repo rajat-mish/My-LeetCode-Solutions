eb793e1560fdb282bd2c61c895031a91038c8d38
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2026-02-16 05:56:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8 MB (99.85%)

--- 0190-reverse-bits/solution.cpp
+++ 0190-reverse-bits/solution.cpp
@@ -4,9 +4,9 @@ public:
         uint32_t rev=0;
         for(int i=0;i<=31;i++){
             uint32_t bit =n&1; //last bit nikali n ki
-            rev=rev<<1;
-            rev=rev|bit;
-            n=n>>1; 
+            rev=rev<<1; // rev to left shift kiya 
+            rev=rev|bit; // rev me vo last bit ka or kar diya 
+            n=n>>1;  // next bit k liye n me right shift mara
         }
         return rev;
     }
