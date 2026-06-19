f9df780def356e75a40f0037f8e944cc22240f07
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2026-06-19 07:59:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.8 MB (73.07%)

--- 1833-find-the-highest-altitude/solution.cpp
+++ 1833-find-the-highest-altitude/solution.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        vector<int>v(gain.size()+1);
-        v[0]=0;
+        int ans=INT_MIN;
+        int pre=0;
         for(int i=0;i<gain.size();i++){
-           // v.push_back(v[i]+gain[i]);
-           v[i+1]=v[i]+gain[i];
+            ans=max(ans,gain[i]+pre);
+            pre+=gain[i];
         }
-       int max=v[0];
-        for(int i=0;i<v.size();i++){
-                   if(v[i]>max){
-                       max=v[i];
-                   }
-        }
-        return max;
+        if(ans<0)return 0;
+        return ans;
     }
 };
