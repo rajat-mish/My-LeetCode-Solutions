8d4abaec5042398b9a5f8f59863027081e06849e
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2026-06-22 11:14:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.7 MB (40.74%)

--- 0162-find-peak-element/solution.cpp
+++ 0162-find-peak-element/solution.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
+       int n=nums.size();
         if(nums.size()==1)return 0;
-        int low=0,high=nums.size()-1;
+         if(nums[0]>nums[1])return 0;
+         if(nums[n-1]>nums[n-2])return n-1;
+         
+         int low=1,high=n-2;
+      
         while(low<=high){
-            int mid=low+(high-low)/2;
-          if(mid>0 && mid<nums.size()-1){
-                 if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
-                      return mid;
-            }
-            else if(nums[mid-1]<nums[mid]){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-          }
-          else if(mid==0){
-            if(nums[0]>nums[1]){
-                return 0;
-            }
-            else return 1;
-          }
-          else if(mid==nums.size()-1){
-            if(nums[nums.size()-2]>nums[nums.size()-1]){
-                return nums.size()-2;
-            }
-            else return nums.size()-1;
-          }
+            int mid=(low+high)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])return mid;
+            else if(nums[mid-1]<nums[mid])low=mid+1;
+            else high=mid-1;
         }
-        return -1;
+        return 0;
     }
 };
