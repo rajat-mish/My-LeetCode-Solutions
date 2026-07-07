880fad875493c6319a998c334925f9e18d93a0e2
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2026-07-07 13:53:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 95.2 MB (61.98%)

--- 0004-median-of-two-sorted-arrays/solution.cpp
+++ 0004-median-of-two-sorted-arrays/solution.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
- double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {vector<int>v;
-     int   m=nums1.size()+nums2.size();
-     for(int i=0;i<nums1.size();i++){
-         v.push_back(nums1[i]);
-     }
-     for(int i=0;i<nums2.size();i++){
-         v.push_back(nums2[i]);
-     }
+int i=0,j=0;
+int getmin(vector<int>&nums1,vector<int>&nums2){
+    if(i<nums1.size() && j<nums2.size()){
+        return nums1[i]<nums2[j]?nums1[i++]:nums2[j++];
+    }
+    else if(i<nums1.size())return nums1[i++];
+    else if(j<nums2.size()) return nums2[j++];
 
-     sort(v.begin(),v.end());
+    return -1;
+}
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        int m=nums2.size();
 
-     if(v.size()%2==0){
-         double d=(v[m/2]+v[m/2 -1])/2.0;
-         return d;
-     }
-     
-     return v[m/2]/1.0;
-        
+        if((n+m)%2==0){
+            for(int i=0;i<(n+m)/2-1;i++){
+                int gb=getmin(nums1,nums2);
+            }
+            return (double)(getmin(nums1,nums2)+getmin(nums1,nums2))/2.0;
+        }
+        else{
+            for(int i=0;i<(n+m)/2;i++){
+                int gb=getmin(nums1,nums2);
+            }
+            return (double)getmin(nums1,nums2);
+        }
+        return -1;
     }
 };
