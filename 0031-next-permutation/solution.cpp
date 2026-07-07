61c0905801c269de44510d66d1349e37c2942689
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2026-07-07 16:47:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.6 MB (75.13%)

--- 0031-next-permutation/solution.cpp
+++ 0031-next-permutation/solution.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n=nums.size();
-        int j= -1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                j=i;
-                break;
-            }
+        int i=nums.size()-2;
+        while(i>=0 && nums[i]>=nums[i+1])i--;
+
+        if(i>=0){
+            int j=nums.size()-1;
+            while(nums[i]>=nums[j])j--;
+            swap(nums[i],nums[j]);
         }
-        if(j==-1){
-            reverse(nums.begin(),nums.end());
-            return;
-        }
-        reverse(nums.begin()+1+j, nums.end());
-        int p=-1;
-        for(int i=j+1;i<n;i++){
-            if(nums[i]>nums[j]){
-                p=i;
-                break;
-            }
-        }
-        int temp=nums[j];
-        nums[j]=nums[p];
-        nums[p]=temp;
-        return;
+        reverse(nums.begin()+i+1,nums.end());
     }
 };
