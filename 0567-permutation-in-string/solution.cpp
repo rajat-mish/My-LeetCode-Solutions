a804a8eb0e7aeec00503c052f229a072ffa10d19
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2026-08-02 07:39:46 +0000

    Sync LeetCode submission Runtime - 10 ms (30.93%), Memory - 10.8 MB (23.61%)

--- 0567-permutation-in-string/solution.cpp
+++ 0567-permutation-in-string/solution.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-bool match(int arr1[],int arr2[]){
-       for(int i=0;i<26;i++){
-           if(arr1[i]!=arr2[i])return false;
-       }
-       return true;
-}
     bool checkInclusion(string s1, string s2) {
-       int j=s1.size()-1;
-       int arr1[26],arr2[26];
-       for(auto x:s1){
-        arr1[x-'a']++;
-       }
+        int n=s1.size();
+        int m=s2.size();
+        if(n>m)return false;
+        int i=0,j=n-1;
+   unordered_map<char,int>mp,mp1;
+   for(int k=0;k<n;k++){
+    mp1[s1[k]]++;
+   }
+   for(int k=i;k<=j;k++)mp[s2[k]]++;
 
-     for(int i=0;i<s2.size();i++){
-        arr2[s2[i]-'a']++;
-        if(i>=j){
-            if(match(arr1,arr2))return true;
-            arr2[s2[i-j]-'a']--;
+        while(j<m){
+          if(mp==mp1)return true;
+          else{
+            mp[s2[i]]--;
+            if(mp[s2[i]]==0)mp.erase(s2[i]);
+            i++;
+            j++;
+            mp[s2[j]]++;
+          }
         }
-     }
-     return false;
-        
+        if(mp==mp1)return true;
+        return false;
     }
 };
