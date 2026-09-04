e69ebc63668d83703858ba1dbfe5beb8170ccfb1
Rajat Mishra <134710574+rajat-mish@users.noreply.github.com>
2026-09-04 07:41:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.4 MB (88.94%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) { vector<int>ans;
-    bool f=false;
+    // bool f=false;
 
-    if(nums.size()==0){
-        ans.push_back(-1);
-        ans.push_back(-1);
-        return ans;
-    }
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]==target){
-            f=true;
-            break;
-        }
-    }
-    if(f==false){
-         ans.push_back(-1);
-        ans.push_back(-1);
-        return ans;
-    }
+    // if(nums.size()==0){
+    //     ans.push_back(-1);
+    //     ans.push_back(-1);
+    //     return ans;
+    // }
+    // for(int i=0;i<nums.size();i++){
+    //     if(nums[i]==target){
+    //         f=true;
+    //         break;
+    //     }
+    // }
+    // if(f==false){
+    //      ans.push_back(-1);
+    //     ans.push_back(-1);
+    //     return ans;
+    // }
        int l=0;
     int h=nums.size()-1;
     int res;
@@ -36,7 +36,7 @@ public:
         h=m-1;
        } 
         } 
-        ans.push_back(l);
+     if((l>=0 && l<nums.size())&& nums[l]==target)   ans.push_back(l);
  //last occur
 
  int p=0;
@@ -55,7 +55,8 @@ public:
             p=mid+1;
         }
     }
-ans.push_back(q);
+if( (q>=0 && q<nums.size())&& nums[q]==target) ans.push_back(q);
+if(ans.size()==0)return {-1,-1};
 return ans;
     }
 };
